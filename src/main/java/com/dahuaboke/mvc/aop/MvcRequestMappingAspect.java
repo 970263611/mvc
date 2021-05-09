82aa6f3b78c43e0abb2a714386820cd056dadfb6
dahua <970263611@qq.com>
2021-05-09 22:05:40 +0800

    优化掉无用代码

--- src/main/java/com/dahuaboke/mvc/aop/MvcRequestMappingAspect.java
+++ src/main/java/com/dahuaboke/mvc/aop/MvcRequestMappingAspect.java
@@ -1,15 +1,10 @@
 package com.dahuaboke.mvc.aop;
 
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
-
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Method;
 
 /**
  * @Author dahua
@@ -31,13 +26,7 @@ public class MvcRequestMappingAspect {
 
     @Around("point()")
     public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
-        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-        Method method = signature.getMethod();
-        Annotation[][] parameterAnnotations = method.getParameterAnnotations();
-        System.out.println(1);
         Object[] args = joinPoint.getArgs();
-        for(Object obj:args){
-        }
         return joinPoint.proceed(args);
     }
 
